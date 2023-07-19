39f4dc4391a106f9904e1e22b7dd04be8437ea4b
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2023-07-18 19:40:48 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,32 +7,38 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k, m;
+	int n2, m, p;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (n2 = 0; n2 <= n; n2++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+
+			for (m = 1; m <= n; m++)
 			{
-				k = i * j / 10;
-				m = i * j % 10;
-				if (k == 0)
+				_putchar(',');
+				_putchar(' ');
+
+				p = n2 * m;
+
+				if (p <= 99)
+					_putchar(' ');
+				if (p <= 9)
+					_putchar(' ');
+
+				if (p >= 100)
 				{
-					_putchar(' ');
-					_putchar(m + 48);
-					_putchar(',');
-					_putchar(' ');
+					_putchar((p / 100) + '0');
+					_putchar(((p / 10)) % 10 + '0');
 				}
-				else
+				else if (p <= 99 && p >= 10)
 				{
-					_putchar(k + 48);
-					_putchar(m + 48);
-					_putchar(',');
-					_putchar(' ');
+					_putchar((p / 10) + '0');
 				}
+				_putchar((p % 10) + '0');
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }

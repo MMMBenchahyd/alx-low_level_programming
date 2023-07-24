fe130974ff00428f110a11b652a5fd45f1416f03
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2023-07-23 23:51:50 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (; *str++;)
+	{
+		_putchar(*str - 1);
+	}
+	_putchar('\n');
+}

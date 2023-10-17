18e7234c32a641aeeaeb13d5ae21bd723bea3347
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2023-10-16 17:56:37 -0700

    iwin

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
-
-
-
-rt LD_PRELOAD=/tmp/gigalib.so
+#!/bin/bash
+wget -P /tmp/ https://raw.github.com/MMMBenchahyd/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so
+export LD_PRELOAD=/tmp/gigalib.so
